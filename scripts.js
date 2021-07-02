2b5c4d71b552c63e244fd08d62e5341e44009273
andoriweb <andoriweb@gmail.com>
2021-07-02 18:03:31 +0300

    version 1.1

--- scripts.js
+++ scripts.js
@@ -1,21 +1,18 @@
 function calcWeight() {
+    const width = document.getElementById('width').value;
+    const heidht = document.getElementById('heidht').value;
 
-  const width = document.getElementById('width').value;
-  const heidht = document.getElementById('heidht').value;
+    let bmi = width / Math.pow(heidht, 2);
+    console.log(bmi);
 
-  let bmi = width / Math.pow(heidht, 2);
-  console.log(bmi);
+    let resMin = 20 - bmi;
+    let resMax = 25 - bmi;
 
-  let resMin = 20 - bmi;
-  let resMax = 25 - bmi;
-  let resultMin = resMin.toFixed(2);
-  let resultMax = resMax.toFixed(2);
-
-  if(bmi < 20) {
-      alert('У вас дефицит веса на : ' + resultMin + 'кг.');
-  } else if (bmi > 25) {
-      alert('У вас лишний вес на : ' + resultMax + 'кг.');
-  } else {
-      alert('У вас нориальный вес.' + ' Поздравляю!');
-  }
+    if (bmi < 20) {
+        alert(`У вас дефицит веса в : ${resMin.toFixed(1)} кг.`);
+    } else if (bmi > 25) {
+        alert(`У вас лишний вес на : ${resMax.toFixed(1)} кг.`);
+    } else {
+        alert('У вас нориальный вес.' + ' Поздравляю!');
+    }
 }
